95174bbba11d1f970a5d0b793e3f3e6cc6d61941
Luan Souza <luan52152510@gmail.com>
2024-10-16 15:47:53 -0300

    condicional

---
+++ script.js
@@ -0,0 +1,19 @@
+// //let nome = prompt("qual é o seu nome?");
+
+// //alert("seja bem vindo(a)," + nome);
+
+// let idade = prompt("Quantos anos você tem ?")
+
+// if(idade >= 18) {
+//     alert("Você já pode tirar a carteira de motorista!!!")
+// }else{
+//     alert("VocÊ já pode tirar a carteira!")
+// }
+
+let gostarDePizza = prompt("VocÊ gosta de pizza? responda (sim ou não?)");
+
+if (gostarDePizza === "sim"){
+    alert("que ótimo! VAmos comer juntos!")
+}else {
+    alert("tudo bem, talvez você goste de outra comida");
+}
